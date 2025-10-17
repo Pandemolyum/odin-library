11de5849c1b399a0f2b955ccdd4d8b8fa80c02d4
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-17 16:22:26 -0400

    Add table to display books
    A table is easier to visualize compared to the previous format. Also
    add a button to delete entries, but it is currently not functional.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const library = [];
 
-addBookToLibrary("Gospel of Biff", "Chris", 420);
+addBookToLibrary("Gospel of Biff", "Christopher Moore", 420);
 addBookToLibrary("Gospel of John", "Boring Dude", 200);
 addBookToLibrary("Gospel of Mark", "Markyboy", 69);
 
 displayLibrary(library);
 
 function Book(title, author, pages) {
-    this.id = crypto.randomUUID();
+    this.uuid = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -19,18 +19,20 @@ function addBookToLibrary(title, author, pages) {
 
 function displayLibrary(lib) {
     for (book of lib) {
-        const newDiv = document.createElement("div");
-        const title = document.createElement("h1");
-        const author = document.createElement("p");
-        const pages = document.createElement("p");
-        
-        title.textContent = book.title;
-        author.textContent = `Author: ${book.author}`;
-        pages.textContent = `Number of pages: ${book.pages}`;
+        const table = document.querySelector('table');
+        const newRow = table.insertRow();
+        const cellTitle = newRow.insertCell();
+        const cellAuthor = newRow.insertCell();
+        const cellPages = newRow.insertCell();
+        const cellButton = newRow.insertCell();
+        const button = document.createElement('button')
+        button.id = 'delete';
+        button.textContent = '✖';
+        button.setAttribute('uuid',book.uuid);
 
-        newDiv.appendChild(title);
-        newDiv.appendChild(author);
-        newDiv.appendChild(pages);
-        document.body.appendChild(newDiv);
+        cellTitle.textContent = book.title;
+        cellAuthor.textContent = book.author;
+        cellPages.textContent = book.pages;
+        cellButton.appendChild(button);
     }
 }
